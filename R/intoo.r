899e2cc9737765930e1eb93a47eb9ec01fa79201
Abby Spurdle <spurdle.a@gmail.com>
2020-03-06 20:40:02 +0000

    version 0.4.0

--- R/intoo.r
+++ R/intoo.r
@@ -1,11 +1,32 @@
+#intoo: Minimal Language-Like Extensions
+#Copyright (C), Abby Spurdle, 2020
+
+#This program is distributed without any warranty.
+
+#This program is free software.
+#You can modify it and/or redistribute it, under the terms of:
+#The GNU General Public License, version 2, or (at your option) any later version.
+
+#You should have received a copy of this license, with R.
+#Also, this license should be available at:
+#https://cran.r-project.org/web/licenses/GPL-2
+
 "%$%" = function (object, name)
 	attr (object, as.character (substitute (name) ) )
 
 "%$%<-" = function (object, name, value)
-  "attr<-" (object, as.character (substitute (name) ), value)
+	"attr<-" (object, as.character (substitute (name) ), value)
 
- .LIST = function (..., start=1)
-{	sc = as.list (sys.call (-1) )[-(1:start)]
+"%@%" = function (object, name)
+	get (as.character (substitute (name) ), envir = environment (object) )
+
+"%@%<-" = function (object, name, value)
+{	assign (as.character (substitute (name) ), value, envir = environment (object) )
+	object
+}
+
+.LIST = function (..., .list.start.at=1)
+{	sc = as.list (sys.call (-1) )[-(1:.list.start.at)]
 	L = list (...)
 	names.1 = names (sc)
 	names.2 = as.character (sc)
@@ -23,12 +44,20 @@ LIST = function (...)
 	.LIST (...)
 
 EXTEND = function (object, class, ...)
-{	cl = list (c (class, class (object) ) )
+{	cl = base::class (object)
+	if (! missing (class) && ! is.null (class) )
+		cl = c (class, cl)
+	cl= list (cl)
 	a1 = attributes (object)
 	a1$class = NULL
-	a2 = .LIST (..., start=3)
+	a2 = .LIST (..., .list.start.at=3)
 	attributes (object) = c (class=cl, a1, a2)
 	object
+}
+
+UNPACK = function (x)
+{	list2env (x, parent.frame (1) )
+	invisible (NULL)
 }
 
 THIS = function ()
--- R/object_info.r
+++ R/object_info.r
@@ -1,4 +1,17 @@
-.object.info = function (object, value, private.attributes, public.attributes, values, comments, n)
+#intoo: Minimal Language-Like Extensions
+#Copyright (C), Abby Spurdle, 2020
+
+#This program is distributed without any warranty.
+
+#This program is free software.
+#You can modify it and/or redistribute it, under the terms of:
+#The GNU General Public License, version 2, or (at your option) any later version.
+
+#You should have received a copy of this license, with R.
+#Also, this license should be available at:
+#https://cran.r-project.org/web/licenses/GPL-2
+
+.object.info = function (object, value, private.attributes, public.attributes, values, comment, n)
 {	#print object's description
 	c = class (object) [1]
 	dims = .dim (object)
@@ -22,10 +35,12 @@
 				if (private.attributes && char.1 == "." || public.attributes && char.1 != ".")
 				{	x = . [[i]]
 					c = class (x) [1]
+					if (c == "matrix")
+						c = paste (mode (x), c)
 					dims = .dim (x)
 					x.attributes = attributes (x)
 					x.attributes = .remove.special.attributes (x.attributes)
-					#print attribute dscription
+					#print attribute description
 					cat ("%$% ", names.attributes [i], ", ", c, ", ", paste (dims, collapse=" * "), sep="")
 					if (!is.null (x.attributes) )
 					{	names.x.attributes = names (x.attributes)
@@ -53,19 +68,19 @@
 		}
 	}
 	#print comments
-	if (comments && !is.null (com) )
+	if (comment && !is.null (com) )
 		cat (com, sep="\n")
 }
 
-object.model = function (object,
-	value=FALSE, private.attributes=FALSE, public.attributes=TRUE, values=value, comments=FALSE,
-	n=3L)
-	.object.info (object, value, private.attributes, public.attributes, values, comments, n)
+object.model = function (object, ...,
+	value=FALSE, private.attributes=TRUE, public.attributes=TRUE, attribute.values=value, comment=FALSE,
+	n=3)
+	.object.info (object, value, private.attributes, public.attributes, attribute.values, comment, n)
 
-object.summary = function (object,
-	value=TRUE, private.attributes=FALSE, public.attributes=TRUE, values=value, comments=TRUE,
-	n=6L)
-	.object.info (object, value, private.attributes, public.attributes, values, comments, n)
+object.summary = function (object, ...,
+	value=TRUE, private.attributes=FALSE, public.attributes=TRUE, attribute.values=value, comment=TRUE,
+	n=6)
+	.object.info (object, value, private.attributes, public.attributes, attribute.values, comment, n)
 
 .remove.special.attributes = function (.)
 {	.$class = NULL
@@ -97,7 +112,11 @@ object.summary = function (object,
 }
 
 .print.head = function (object, n)
-{	if (is.list (object) && !is.data.frame (object) )
+{	if (isS4 (object) )
+	{	h = head (object, n)
+		show (h)
+	}
+	else if (is.list (object) && !is.data.frame (object) )
 		.print.head.list (object)
 	else if (is.function (object) )
 	{	attributes (object) = NULL
@@ -106,7 +125,9 @@ object.summary = function (object,
 				cat (l, "\n", sep="")
 	}
 	else
-	{	h = head (object, n)
+	{	if (is.factor (object) )
+			object = as.character (object)
+		h = head (object, n)
 		print (h, quote=FALSE)
 	}
 }
